ef81eb9b13d45b5a7327d9f1ac6e483118f7ffd7
Freddy Barrera Artiaga <fredy.barrera.a@gmail.com>
2020-04-02 22:47:40 -0300

    Se crea la app core

--- webempresa/wsgi.py
+++ webempresa/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for webempresa2 project.
+WSGI config for webempresa project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webempresa2.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webempresa.settings")
 
 application = get_wsgi_application()
