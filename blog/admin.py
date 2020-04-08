622a9aa24348788a230af3d39ff96cc6d1188054
Freddy Barrera Artiaga <fredy.barrera.a@gmail.com>
2020-04-08 01:49:34 -0300

    Se personaliza el admin

--- blog/admin.py
+++ blog/admin.py
@@ -9,6 +9,22 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
+    list_display = ('title', 'author', 'published', 'post_categories')
+    ordering = ('author', 'published')
+    # Puedo hacer busquedas dentro de autor por el campo "username".
+    # Busco ademas por los nombres de las categorias.
+    search_fields = ('title', 'content', 'author__username', 'categories__name') 
+    # Permite jerarquizar por fechas.
+    date_hierarchy = 'published'
+    # Permite añadir filtros de búsqueda
+    list_filter = ('author__username', 'categories__name')
+    
+    # Permite definir lo que se mostrará en la columna, en este caso son los nombres de las categorias separados por coma (,)
+    def post_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all().order_by("name")])
+
+    # Sobreescribe el atributo 'short_description' para agregar un texto mas descriptivo.
+    post_categories.short_description = 'Categorías'
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
