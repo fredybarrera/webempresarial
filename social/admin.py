55f36764ccc4fa6c69c474fac1c13ae5e5b3602e
Freddy Barrera Artiaga <fredy.barrera.a@gmail.com>
2020-04-09 23:04:18 -0400

    Grupos y permisos

--- social/admin.py
+++ social/admin.py
@@ -5,4 +5,11 @@ from .models import Link
 class LinkAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 
+    # Valido si el usuario conectado pertenece al grupo "Personal", no dejo editar los campos 'created', 'updated', 'key', 'name'
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name="Personal").exists():
+            return ('key', 'name')
+        else:
+            return ('created', 'updated')
+
 admin.site.register(Link, LinkAdmin)
